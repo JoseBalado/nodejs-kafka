acdcd937a097e8d536b96dff4e1c997654ff921b
Jose Balado <JoseBalado@users.noreply.github.com>
2018-08-12 00:50:35 +0100

    Read-files-stream read each log file and stores it as a Kafka message

--- read-files-stream.js
+++ read-files-stream.js
@@ -1,7 +1,7 @@
 const Kafka = require('node-rdkafka')
+const fs = require('fs');
 
 const topicName = 'log-files'
-
 const stream = Kafka.Producer.createWriteStream({
   'debug' : 'all',
   //'metadata.broker.list': '172.17.0.1:9092', // only one Kafka broker
@@ -37,11 +37,43 @@ stream.producer.on('ready', (value1, value2) => {
   })
 })
 
-const maxMessages = 10
-for (var i = 0; i < maxMessages; i++) {
-  const value = new Buffer.from(`{"previousURL":"^","currentURL":"/wireless","currentParams":{},"timeStamp":"2018-06-19T12:38:0${i}.TZ-2"}`)
-  stream.write(value)
-}
+
+// Store file data chunks in this array
+const chunks = [];
+
+// Read file into stream.Readable
+const fileStream = fs.createReadStream('logs/0e50c350f704b1771a8b706f2c946eba_linux_Data_log-2018-04-05T08-13-09TZ+0.txt')
+
+// An error occurred with the stream
+fileStream.once('error', (err) => {
+  // Be sure to handle this properly!
+  console.error(err)
+})
+
+// Data is flushed from fileStream in chunks,
+// this callback will be executed for each chunk
+fileStream.on('data', (chunk) => {
+  chunks.push(chunk); // push data chunk to array
+  // We can perform actions on the partial data we have so far!
+})
+
+// File is done being read
+fileStream.once('end', () => {
+  // create the final data Buffer from data chunks;
+  stream.write(Buffer.concat(chunks))
+  // Of course, you can do anything else you need to here, like emit an event!
+})
+
+//readStream.on('data', data => {
+//  console.log('Got message')
+//  writeStream.write(Buffer.from(data.toString().replace(/TZ[+-][0-9]{0,2}/g, "000Z")))
+//})
+
+//const maxMessages = 10
+//for (var i = 0; i < maxMessages; i++) {
+//  const value = new Buffer.from(`{"previousURL":"^","currentURL":"/wireless","currentParams":{},"timeStamp":"2018-06-19T12:38:0${i}.TZ-2"}`)
+//  stream.write(value)
+//}
 
 stream.on('error', function (err) {
   // Here's where we'll know if something went wrong sending to Kafka
